2fbc614bb8a688b4d8364f9210b160f8744ae383
jrafaaael <jrmd070102@gmail.com>
2023-01-01 14:28:11 -0400

    move `projects` to `data` folder

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,13 +9,7 @@ const projects = [
   {
     title: "ecg",
     description: `visualize the ECG graph in different devices simultaneously in real time through WiFi in the patient/doctor's preferred browser. developed for the subject "Instrumentación Médica IV" and chosen to be exposed at the "Expo Investigación Universitaria 2022"`,
-    stack: [
-      "javascript",
-      "nodejs",
-      "express",
-      "socket.io",
-      "arduino",
-    ],
+    stack: ["javascript", "nodejs", "express", "socket.io", "arduino"],
     img: "/ecg.jpg",
   },
   {
@@ -35,6 +29,12 @@ const projects = [
     title: "school-bell-automation",
     description: `automation of the school bell of "Complejo Educativo Fray Luís Amigó" for each start of class, recess and end of classes of each section and grade`,
     stack: ["arduino"],
+    img: "/arduino.jpeg",
+  },
+  {
+    title: "sigi",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nostrum eius, nemo iste iure odit voluptates vel quisquam ad sint ullam, placeat dolore aliquid voluptatem corrupti necessitatibus magnam ea quia.`,
+    stack: ["react", "inertia", "php", "laravel"],
     img: "/arduino.jpeg",
   },
 ];
