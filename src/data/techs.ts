e41bd08e63662394ef7bfc78132ab5b8b26bd458
jrafaaael <jrmd070102@gmail.com>
2023-01-05 15:46:16 -0400

    add border styling

--- src/data/techs.ts
+++ src/data/techs.ts
@@ -14,20 +14,23 @@ export interface Tech {
   icon: any;
   fg?: string;
   bg?: string;
+  bd?: string;
 }
 
 export const JAVASCRIPT: Tech = {
   name: "JavaScript",
   icon: JavaScript,
   fg: "text-javascript",
-  bg: "bg-javascript/10",
+  bg: "bg-javascript/20",
+  bd: "border-javascript/40",
 };
 
 export const NODE: Tech = {
   name: "NodeJS",
   icon: Node,
   fg: "text-node",
-  bg: "bg-node/10",
+  bg: "bg-node/20",
+  bd: "border-node/40",
 };
 
 export const EXPRESS: Tech = {
@@ -43,46 +46,49 @@ export const SOCKETIO: Tech = {
 export const ARDUINO: Tech = {
   name: "Arduino",
   icon: Arduino,
-  fg: "text-arduino",
-  bg: "bg-arduino/10",
+  bg: "bg-arduino/40",
+  bd: "border-arduino/40",
 };
 
 export const PYTHON: Tech = {
   name: "Python",
   icon: Python,
   fg: "text-python",
-  bg: "bg-python/10",
+  bg: "bg-python/20",
+  bd: "border-python/40",
 };
 
 export const REACT: Tech = {
   name: "React",
   icon: React,
   fg: "text-react",
-  bg: "bg-react/10",
+  bg: "bg-react/20",
+  bd: "border-react/40",
+};
+
+export const TAILWIND: Tech = {
+  name: "Tailwind",
+  icon: Tailwind,
+  bg: "bg-tailwind/40",
+  bd: "border-tailwind/40",
 };
 
 export const REACT_NATIVE: Tech = {
   name: "React Native",
   icon: React,
   fg: "text-react",
-  bg: "bg-react/10",
-};
-
-export const TAILWIND: Tech = {
-  name: "Tailwind",
-  icon: Tailwind,
-  fg: "text-tailwind",
-  bg: "bg-tailwind/10",
-};
-
-export const EXPO: Tech = {
-  name: "Expo",
-  icon: Expo,
+  bg: "bg-react/20",
+  bd: "border-react/40",
 };
 
 export const TYPESCRIPT: Tech = {
   name: "TypeScript",
   icon: TypeScript,
-  fg: "text-typescript",
-  bg: "bg-typescript/10",
+  bg: "bg-typescript/40",
+  bd: "border-typescript/40",
+};
+
+export const EXPO: Tech = {
+  name: "Expo",
+  icon: Expo,
 };
