65221d8529e86f13ffa5ab65f4354f942ec7f098
jrafaaael <jrmd070102@gmail.com>
2023-02-05 16:33:20 -0400

    add social links

---
+++ src/data/social-links.ts
@@ -0,0 +1,27 @@
+import GitHub from "../components/icons/GitHub.astro";
+import LinkedIn from "../components/icons/LinkedIn.astro";
+import Twitter from "../components/icons/Twitter.astro";
+import At from "../components/icons/At.astro";
+
+export const links = [
+  {
+    label: "GitHub",
+    icon: GitHub,
+    link: "https://github.com/jrafaaael/",
+  },
+  {
+    label: "LinkedIn",
+    icon: LinkedIn,
+    link: "#",
+  },
+  {
+    label: "Twitter",
+    icon: Twitter,
+    link: "#",
+  },
+  {
+    label: "Email",
+    icon: At,
+    link: "#",
+  },
+];
