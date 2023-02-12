33e0c6290d8c33c0ef96a1570780509d3afee028
jrafaaael <jrmd070102@gmail.com>
2023-02-11 23:46:24 -0400

    fix grammar

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -24,7 +24,7 @@ export interface Project {
 export const projects: Project[] = [
   {
     title: "ecg",
-    description: `hardware and software development to visualize ECG plot in multiple devices simultaneously in real-time through WiFi in patient/doctor's preferred browser. developed for the subject "Instrumentación Médica IV" and chosen to be exposed at "Expo Investigación Universitaria 2022"`,
+    description: `Hardware and software development to visualize ECG plot in multiple devices simultaneously in real-time through WiFi in patient/doctor's preferred browser.`,
     shortDescription:
       "visualize ECG plot in multiple devices simultaneously in real-time through WiFi in patient/doctor's preferred browser.",
     stack: [JAVASCRIPT, NODE, EXPRESS, SOCKETIO, ARDUINO],
@@ -32,7 +32,7 @@ export const projects: Project[] = [
   },
   {
     title: "sigi",
-    description: `internal tool to share files on local network and generate "Plan Operativo Anual" (POA) document. also allow internal comunication between teams through real-time chat. developed for CMSF`,
+    description: `Internal tool to share files on local network and generate "Plan Operativo Anual" (POA) document. Also allow internal comunication between teams through real-time chat.`,
     shortDescription:
       "internal tool to share files on local network, generate POA and chat in real-time.",
     stack: [JAVASCRIPT, REACT, TAILWIND, SOCKETIO],
@@ -41,23 +41,23 @@ export const projects: Project[] = [
   {
     title: "tw-rt-report",
     description:
-      "script to generate a spreadsheet with all retweets made by a list of users to @account in the current month. developed for CMSF",
+      "Python script to generate a report spreadsheet with all retweets made by a list of users to @account in the current month.",
     shortDescription:
-      "track retweets made by a list of users to @account in the current month.",
+      "script to track retweets made by a list of users to @account in the current month.",
     stack: [PYTHON],
     img: "/images/python_code.webp",
   },
   {
     title: "school-bell-automation",
-    description: `automation of "Complejo Educativo Fray Luís Amigó"\`s school bell for each start of classes, recess and end of classes of each section and grade`,
+    description: `School bell automation of "Complejo Educativo Fray Luís Amigó" for each start of classes, recess and end of classes of each course.`,
     shortDescription:
-      "school bell automation for each start of classes, recess and end of classes of each section and grade.",
+      "school bell automation for each start of classes, recess and end of classes of each course.",
     stack: [ARDUINO],
     img: "/images/arduino.webp",
   },
   {
     title: "moodhub",
-    description: `app to track emotions daily.`,
+    description: `App to track emotions daily.`,
     shortDescription: null,
     stack: [TYPESCRIPT, REACT_NATIVE, EXPO],
     img: null,
