cc7a204b43ad734f0782d84b70faa9aad8276ebc
jrafaaael <jrmd070102@gmail.com>
2023-04-15 21:40:07 -0400

    add `mdx` integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
