426634e9a9dd7a8b158574d62da001c7709bd984
jrafaaael <jrmd070102@gmail.com>
2023-04-18 10:55:33 -0400

    add social links in index page instead of footer

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -1,27 +1,22 @@
-import GitHub from "../components/icons/GitHub.astro";
-import LinkedIn from "../components/icons/LinkedIn.astro";
-import Twitter from "../components/icons/Twitter.astro";
-import At from "../components/icons/At.astro";
-
 export const links = [
   {
     label: "GitHub",
-    icon: GitHub,
     link: "https://github.com/jrafaaael/",
+    text: "See me code"
   },
   {
     label: "LinkedIn",
-    icon: LinkedIn,
     link: "https://www.linkedin.com/in/jrafaaael/",
+    text: "Hire me!"
   },
   {
     label: "Twitter",
-    icon: Twitter,
     link: "https://twitter.com/_jrafaaael",
+    text: "Follow me"
   },
   {
     label: "Email",
-    icon: At,
     link: "mailto:jrmd070102@gmail.com",
+    text: "Let's chat!"
   },
 ];
