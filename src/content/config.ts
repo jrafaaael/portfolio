2a6abba86804ff6178e930deac5b1ec86b7d86c3
jrafaaael <jrmd070102@gmail.com>
2023-04-17 21:56:14 -0400

    remove content collections

--- src/content/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection } from "astro:content";
-import { projectSchema } from "./project/schema";
-
-const projectCollection = defineCollection(projectSchema);
-
-export const collections = {
-  project: projectCollection,
-};
